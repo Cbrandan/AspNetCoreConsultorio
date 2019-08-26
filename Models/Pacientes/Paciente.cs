a183a88b349cd5cd4aef3dd9833872d83af0f36f
Cristian Brandan <Cristian.Brandan@yahoo.com>
2019-08-25 21:06:24 -0300

    ClaseAbstracta

--- Models/Pacientes/Paciente.cs
+++ Models/Pacientes/Paciente.cs
@@ -4,13 +4,13 @@ namespace AspNetCoreConsultorio.Models
 {
 public class Paciente : Persona
 {
-    public DateTimeOffset? Fecha_Alta { get; set; }
-    public Guid IdPaciente { get; set; }
-
-        public Paciente(int dni, string apellido, string nombre, char sexo, DateTimeOffset? fecha_nacimiento, DateTimeOffset? fecha_alta)
-        : base(dni, apellido, nombre, sexo, fecha_nacimiento)
-        {
-            this.Fecha_Alta = fecha_alta;
-        }
+    public DateTimeOffset? Fecha_Alta { get; set; }   
+    
+    public Paciente(int dni, string apellido, string nombre, char sexo,
+                    DateTimeOffset fecha_nacimiento, DateTimeOffset fecha_alta)
+            : base(dni, apellido, nombre, sexo, fecha_nacimiento)
+    {
+        this.Fecha_Alta = fecha_alta;
     }
+}
 }
--- Models/Pacientes/Persona.cs
+++ Models/Pacientes/Persona.cs
@@ -4,14 +4,15 @@ namespace AspNetCoreConsultorio.Models
 {
 public abstract class Persona
 {
+    public Guid Id { get; set; }
     [Required]
     public int DNI { get; set; }
     public string Apellido { get; set; }
     public string Nombre { get; set; }
     public char Sexo { get; set; }
-    public DateTimeOffset? Fecha_Nacimiento { get; set; }
+    public DateTimeOffset Fecha_Nacimiento { get; set; }
 
-    protected Persona(int dni, string apellido, string nombre, char sexo, DateTimeOffset? fecha_nacimiento)
+       protected Persona(int dni, string apellido, string nombre, char sexo, DateTimeOffset fecha_nacimiento)
     {
         DNI = dni;
         Apellido = apellido;
